6d06ddcdcdbfd2ded77fbd2d41a60106638e1e7a
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-09-18 07:28:54 +0600

    Added middleware to intercept protected routes

---
+++ src/middleware.ts
@@ -0,0 +1,46 @@
+/* eslint-disable prettier/prettier */
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+import { getCurrentUser } from "./services/AuthService";
+
+const AuthRoutes = ["/login", "/register"];
+
+type Role = keyof typeof roleBasedRoutes;
+
+const roleBasedRoutes = {
+  USER: [/^\/profile/],
+  ADMIN: [/^\/admin/],
+};
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  const user = await getCurrentUser();
+
+  if (!user) {
+    if (AuthRoutes.includes(pathname)) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(
+        new URL(`/login?redirect=${pathname}`, request.url)
+      );
+    }
+  }
+
+  if (user?.role && roleBasedRoutes[user?.role as Role]) {
+    const routes = roleBasedRoutes[user?.role as Role];
+
+    if (routes.some((route) => pathname.match(route))) {
+      return NextResponse.next();
+    }
+  }
+
+  return NextResponse.redirect(new URL("/", request.url));
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/profile", "/profile/:page*", "/admin", "/login", "/register"],
+};
