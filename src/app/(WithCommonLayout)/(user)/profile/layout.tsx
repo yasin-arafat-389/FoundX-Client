3cdd1e7b26e561a5c988b939ec51f33e587aa42a
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-09-19 08:54:10 +0600

    Designed layout for profile page

---
+++ src/app/(WithCommonLayout)/(user)/profile/layout.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/order */
+/* eslint-disable prettier/prettier */
+import Container from "@/src/components/UI/Container";
+import Sidebar from "@/src/components/UI/Sidebar";
+import { ReactNode } from "react";
+
+export default function layout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <Container>
+        <div className="my-3 flex w-full gap-12">
+          <div className="w-2/5">
+            <Sidebar />
+          </div>
+          <div className="w-4/5">{children}</div>
+        </div>
+      </Container>
+    </>
+  );
+}
