b8858a025fa4bb805bb69007b72649570074fa9b
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-09-20 08:11:21 +0600

    Created custom hooks for authentication

---
+++ src/hooks/auth.hooks.ts
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/order */
+/* eslint-disable prettier/prettier */
+import { useMutation } from "@tanstack/react-query";
+import { FieldValues } from "react-hook-form";
+import { toast } from "sonner";
+import { loginUser, registerUser } from "../services/AuthService";
+
+export const useUserRegistration = () => {
+  return useMutation<any, Error, FieldValues>({
+    mutationKey: ["USER_REGISTRATION"],
+    mutationFn: async (userData) => await registerUser(userData),
+    onSuccess: () => {
+      toast.success("User registration successful.");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+};
+
+export const useUserLogin = () => {
+  return useMutation<any, Error, FieldValues>({
+    mutationKey: ["USER_LOGIN"],
+    mutationFn: async (userData) => await loginUser(userData),
+    onSuccess: () => {
+      toast.success("User login successful.");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+};
