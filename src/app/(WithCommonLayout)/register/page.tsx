624e9ded3fa309c63aca6085f602762ae50b86a5
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-09-22 08:56:42 +0600

    Designed the registration page

---
+++ src/app/(WithCommonLayout)/register/page.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@nextui-org/button";
+import Link from "next/link";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+
+import FXForm from "@/src/components/form/FXForm";
+import FXInput from "@/src/components/form/FXInput";
+import registerValidationSchema from "@/src/schemas/register.schema";
+import { useUserRegistration } from "@/src/hooks/auth.hooks";
+
+export default function RegisterPage() {
+  const { mutate: handleUserRegistration, isPending } = useUserRegistration();
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const userData = {
+      ...data,
+      profilePhoto:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    };
+
+    handleUserRegistration(userData);
+  };
+
+  if (isPending) {
+    //  handle loading state
+  }
+
+  return (
+    <div className="flex h-[calc(100vh-100px)] flex-col items-center justify-center">
+      <h3 className="my-2 text-xl font-bold">Register with FoundX</h3>
+      <p className="mb-4">Help Lost Items Find Their Way Home</p>
+      <div className="w-[35%]">
+        <FXForm
+          //! Only for development
+          defaultValues={{
+            name: "Mir Hussain",
+            email: "mir@gmail.com",
+            mobileNumber: "01711223344",
+            password: "123456",
+          }}
+          resolver={zodResolver(registerValidationSchema)}
+          onSubmit={onSubmit}
+        >
+          <div className="py-3">
+            <FXInput label="Name" name="name" size="sm" />
+          </div>
+          <div className="py-3">
+            <FXInput label="Email" name="email" size="sm" />
+          </div>
+          <div className="py-3">
+            <FXInput label="Mobile Number" name="mobileNumber" size="sm" />
+          </div>
+          <div className="py-3">
+            <FXInput
+              label="Password"
+              name="password"
+              size="sm"
+              type="password"
+            />
+          </div>
+
+          <Button
+            className="my-3 w-full rounded-md bg-default-900 text-default"
+            size="lg"
+            type="submit"
+          >
+            Registration
+          </Button>
+        </FXForm>
+        <div className="text-center">
+          Already have an account ? <Link href={"/login"}>Login</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
