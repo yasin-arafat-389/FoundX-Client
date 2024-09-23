51d984e67f4cd82bee81cf88a3d8c17d3d9ccac8
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-09-23 08:10:19 +0600

    Created validation schema for login and registration

---
+++ src/schemas/login.schema.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { z } from "zod";
+
+const loginValidationSchema = z.object({
+  email: z.string().trim().email("Please enter a valid email"),
+  password: z
+    .string()
+    .trim()
+    .min(6, "Password needs to be at lest 6 character"),
+});
+
+export default loginValidationSchema;
