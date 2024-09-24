5a74bb6d5d562b671653b67e52a341faa01e7d94
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-09-24 08:14:02 +0600

    Added all necessary providers

---
+++ src/lib/Providers/index.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+"use client";
+
+import * as React from "react";
+import { NextUIProvider } from "@nextui-org/system";
+import { useRouter } from "next/navigation";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProviderProps } from "next-themes/dist/types";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+
+import UserProvider from "@/src/context/user.provider";
+
+export interface ProvidersProps {
+  children: React.ReactNode;
+  themeProps?: ThemeProviderProps;
+}
+const queryClient = new QueryClient();
+
+export function Providers({ children, themeProps }: ProvidersProps) {
+  const router = useRouter();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <NextUIProvider navigate={router.push}>
+          <Toaster />
+          <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+        </NextUIProvider>
+      </UserProvider>
+    </QueryClientProvider>
+  );
+}
