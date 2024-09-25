53d02b34269408f30ca66e875f5b01c824c47ef0
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-09-25 08:00:45 +0600

    Created a service for search item

---
+++ src/services/Search/index.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+"use server";
+
+import axiosInstance from "@/src/lib/AxiosInstance";
+
+export const searchItems = async (searchTerm: string) => {
+  try {
+    const res = await axiosInstance.get(
+      `/search-items?searchTerm=${searchTerm}`
+    );
+
+    return res.data;
+  } catch (error) {
+    throw new Error("Failed to search items");
+  }
+};
