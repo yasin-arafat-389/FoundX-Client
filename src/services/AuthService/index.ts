75c342c2f724e507fba9433ce4ca6115315a5524
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-09-26 08:39:21 +0600

    Added all necessary endpoints to auth services

--- src/services/AuthService/index.ts
+++ src/services/AuthService/index.ts
@@ -3,6 +3,7 @@
 
 import { cookies } from "next/headers";
 import { FieldValues } from "react-hook-form";
+import { jwtDecode } from "jwt-decode";
 
 import axiosInstance from "@/src/lib/AxiosInstance";
 
@@ -18,5 +19,66 @@ export const registerUser = async (userData: FieldValues) => {
     return data;
   } catch (error: any) {
     throw new Error(error);
+  }
+};
+
+export const loginUser = async (userData: FieldValues) => {
+  try {
+    const { data } = await axiosInstance.post("/auth/login", userData);
+
+    if (data.success) {
+      cookies().set("accessToken", data?.data?.accessToken);
+      cookies().set("refreshToken", data?.data?.refreshToken);
+    }
+
+    return data;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
+
+export const logout = () => {
+  cookies().delete("accessToken");
+  cookies().delete("refreshToken");
+};
+
+export const getCurrentUser = async () => {
+  const accessToken = cookies().get("accessToken")?.value;
+
+  let decodedToken = null;
+
+  if (accessToken) {
+    decodedToken = await jwtDecode(accessToken);
+
+    return {
+      _id: decodedToken._id,
+      name: decodedToken.name,
+      email: decodedToken.email,
+      mobileNumber: decodedToken.mobileNumber,
+      role: decodedToken.role,
+      status: decodedToken.status,
+      profilePhoto: decodedToken.profilePhoto,
+    };
+  }
+
+  return decodedToken;
+};
+
+export const getNewAccessToken = async () => {
+  try {
+    const refreshToken = cookies().get("refreshToken")?.value;
+
+    const res = await axiosInstance({
+      url: "/auth/refresh-token",
+      method: "POST",
+      withCredentials: true,
+      headers: {
+        cookies: `refreshToken=${refreshToken}`,
+      },
+    });
+
+    return res.data;
+  } catch (error) {
+    throw new Error("Failed to get new access token");
   }
 };
