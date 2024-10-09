69d7b22ac55d2f466f5ff317daea16ddf3033ae7
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-10-09 07:52:30 +0600

    created a server action to fetch recent posts

--- src/services/RecentPosts/index.ts
+++ src/services/RecentPosts/index.ts
@@ -3,8 +3,15 @@
 import envConfig from "@/src/config/envConfig";
 
 export const getRecentPosts = async () => {
+  const fetchOption = {
+    next: {
+      tags: ["posts"],
+    },
+  };
+
   const res = await fetch(
-    `${envConfig.baseApi}/items?sortBy=-createdAt&limit=9`
+    `${envConfig.baseApi}/items?sortBy=-createdAt&limit=9`,
+    fetchOption
   );
 
   return res.json();
