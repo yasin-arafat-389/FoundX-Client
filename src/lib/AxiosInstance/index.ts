43e9746e361f479bda2bcd6e2e8fb8ca270cfca9
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-10-11 07:45:26 +0600

    Created an instance of axios with base api and configured axios interceptor

---
+++ src/lib/AxiosInstance/index.ts
@@ -0,0 +1,50 @@
+/* eslint-disable prettier/prettier */
+import axios from "axios";
+import { cookies } from "next/headers";
+
+import envConfig from "@/src/config/envConfig";
+import { getNewAccessToken } from "@/src/services/AuthService";
+
+const axiosInstance = axios.create({
+  baseURL: envConfig.baseApi,
+});
+
+axiosInstance.interceptors.request.use(
+  function (config) {
+    const cookieStore = cookies();
+    const accessToken = cookieStore.get("accessToken")?.value;
+
+    if (accessToken) {
+      config.headers.Authorization = accessToken;
+    }
+
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  async function (error) {
+    const config = error.config;
+
+    if (error?.response?.status === 401 && !config?.sent) {
+      config.sent = true;
+      const res = await getNewAccessToken();
+      const accessToken = res.data.accessToken;
+
+      config.headers["Authorization"] = accessToken;
+      cookies().set("accessToken", accessToken);
+
+      return axiosInstance(config);
+    } else {
+      return Promise.reject(error);
+    }
+  }
+);
+
+export default axiosInstance;
