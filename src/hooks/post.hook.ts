2beb2541e19c8ffff68720fd60e41c17914f21d4
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-10-13 09:45:35 +0600

    created a hook for posting found item

---
+++ src/hooks/post.hook.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+import { createPost } from "../services/Post";
+
+export const useCreatePost = () => {
+  return useMutation<any, Error, FormData>({
+    mutationKey: ["CREATE_POST"],
+    mutationFn: async (postData) => await createPost(postData),
+    onSuccess: () => {
+      toast.success("Post created successfully");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+};
