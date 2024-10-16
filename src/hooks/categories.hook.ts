2155e35405939aae88ef7b48a07f277bfcaf77e7
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-10-16 07:30:17 +0600

    created a hook to get categories

---
+++ src/hooks/categories.hook.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { useQuery } from "@tanstack/react-query";
+
+import { getCategories } from "../services/Categories";
+
+export const useGetCategories = () => {
+  return useQuery({
+    queryKey: ["GET_CATEGORIES"],
+    queryFn: async () => await getCategories(),
+  });
+};
