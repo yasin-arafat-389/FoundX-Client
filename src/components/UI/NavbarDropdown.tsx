82ed69f17b89bd483e66d151fd083f06e1c0d2dc
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-10-18 08:52:05 +0600

    Designed navbar dropdown

---
+++ src/components/UI/NavbarDropdown.tsx
@@ -0,0 +1,61 @@
+/* eslint-disable prettier/prettier */
+"use client";
+
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@nextui-org/dropdown";
+import { usePathname, useRouter } from "next/navigation";
+import { Avatar } from "@nextui-org/avatar";
+
+import { logout } from "@/src/services/AuthService";
+import { useUser } from "@/src/context/user.provider";
+import { protectedRoutes } from "@/src/constant";
+
+export default function NavbarDropdown() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const { user, setIsLoading: userLoading } = useUser();
+
+  const handleLogout = () => {
+    logout();
+    userLoading(true);
+
+    if (protectedRoutes.some((route) => pathname.match(route))) {
+      router.push("/");
+    }
+  };
+
+  const handleNavigation = (pathname: string) => {
+    router.push(pathname);
+  };
+
+  return (
+    <Dropdown>
+      <DropdownTrigger>
+        <Avatar className="cursor-pointer" src={user?.profilePhoto} />
+      </DropdownTrigger>
+      <DropdownMenu aria-label="Static Actions">
+        <DropdownItem onClick={() => handleNavigation("/profile")}>
+          Profile
+        </DropdownItem>
+        <DropdownItem onClick={() => handleNavigation("/profile/settings")}>
+          Settings
+        </DropdownItem>
+        <DropdownItem onClick={() => handleNavigation("/profile/create-post")}>
+          Create Post
+        </DropdownItem>
+        <DropdownItem
+          key="delete"
+          className="text-danger"
+          color="danger"
+          onClick={() => handleLogout()}
+        >
+          Logout
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
