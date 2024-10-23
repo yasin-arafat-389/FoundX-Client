0a74e7bd3d6f031f1eae06ebabc254e71b86d3a8
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-10-23 08:18:39 +0600

    Designed the sidebar

---
+++ src/components/UI/Sidebar/Constants.ts
@@ -0,0 +1,7 @@
+export const userLinks = [
+  { href: "/profile", label: "Posts" },
+  { href: "/profile/received-claim-requests", label: "Received Claim Request" },
+  { href: "/profile/settings", label: "Settings" },
+];
+
+export const adminLinks = [{ href: "/admin", label: "Admin" }];
---
+++ src/components/UI/Sidebar/index.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { Button } from "@nextui-org/button";
+import Link from "next/link";
+import Image from "next/image";
+
+import { SidebarOptions } from "./SidebarOptions";
+import { adminLinks, userLinks } from "./Constants";
+
+import { useUser } from "@/src/context/user.provider";
+
+const Sidebar = () => {
+  const { user } = useUser();
+
+  return (
+    <div>
+      <div className="rounded-xl bg-default-100 p-2">
+        <div className="h-[330px] w-full rounded-md">
+          <Image
+            alt="profile"
+            className="w-full h-full object-cover rounded-md"
+            height={330}
+            src={user?.profilePhoto as string}
+            width={330}
+          />
+        </div>
+        <div className="my-3">
+          <h1 className="text-2xl font-semibold">{user?.name}</h1>
+          <p className="break-words text-sm">{user?.email}</p>
+        </div>
+        <Button
+          as={Link}
+          className="mt-2 w-full rounded-md"
+          href={"/profile/create-post"}
+        >
+          Create a post
+        </Button>
+      </div>
+      <div className="mt-3 space-y-2 rounded-xl bg-default-100 p-2">
+        <SidebarOptions
+          links={user?.role === "USER" ? userLinks : adminLinks}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
