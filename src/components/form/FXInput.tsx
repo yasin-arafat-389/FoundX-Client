ab2d445aac58ef53d5326fcffccc56b49113d99d
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-11-03 07:41:58 +0600

    Designed a reusable input component

---
+++ src/components/form/FXInput.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable prettier/prettier */
+"use client";
+
+import { Input } from "@nextui-org/input";
+import { useFormContext } from "react-hook-form";
+
+import { IInput } from "@/src/types";
+
+interface IProps extends IInput {}
+
+export default function FXInput({
+  variant = "bordered",
+  size = "md",
+  required = false,
+  type = "text",
+  label,
+  name,
+}: IProps) {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  return (
+    <Input
+      {...register(name)}
+      errorMessage={errors[name] ? (errors[name].message as string) : ""}
+      isInvalid={!!errors[name]}
+      label={label}
+      required={required}
+      size={size}
+      type={type}
+      variant={variant}
+    />
+  );
+}
