1ae5cc46a0e5570270b23f829c30cc05b162c2f5
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-11-07 08:19:29 +0600

    Designed a reusable text area

---
+++ src/components/form/FXTextArea.tsx
@@ -0,0 +1,31 @@
+import { Textarea } from "@nextui-org/input";
+import { useFormContext, useWatch } from "react-hook-form";
+
+import { IInput } from "@/src/types";
+
+interface IProps extends IInput {
+  type?: string;
+}
+
+export default function FXTextarea({
+  name,
+  label,
+  variant = "bordered",
+}: IProps) {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  const currentValue = useWatch({ name });
+
+  return (
+    <Textarea
+      {...register(name)}
+      label={label}
+      minRows={6}
+      value={currentValue || ""}
+      variant={variant}
+    />
+  );
+}
